f80b26c3a6c61e588ff3b0bd23f0e71b5e1ca53e
Phil Johnston <support@mintplugins.com>
2015-09-21 15:20:13 -0400

    Make WooGrid alignment "centered" by default.

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -11,3 +11,20 @@
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @author     Philip Johnston
  */
+ 
+/**
+ * Make WooGrid Content Type Centered by default
+ *
+ * @access   public
+ * @since    1.0.0
+ * @param    $centered_content_types array - An array containing a string for each content-type that should default to centered brick alignment.
+ * @param    $centered_content_types array - An array containing a string for each content-type that should default to centered brick alignment.
+ */
+function mp_stacks_woogrid_centered_by_default( $centered_content_types ){
+	
+	$centered_content_types['woogrid'] = 'woogrid';
+	
+	return $centered_content_types;
+	
+}
+add_filter( 'mp_stacks_centered_content_types', 'mp_stacks_woogrid_centered_by_default' );
